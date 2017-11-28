14b842f9fcff023295f8c574307932c6b05d8771
Hiago Koziel <hiagokoziel100@gmail.com>
2017-11-28 17:43:42 +0000

    Inserting validation fields

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,12 @@ class ArticlesController <ApplicationController
     end
   end
   
-  private def article_params
-    params.require(:article).permit(:title, :description)
+  def show
+    @article = Article.find(params[:id])
   end
+  
+  private 
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
