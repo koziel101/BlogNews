3fd156bb4bcaeeba456fc4e26fd50004ff6824a9
koziel101 <hiago101@hotmail.com>
2017-11-29 00:14:27 -0200

    Installing Bootstrap Bill Turner (reverted from commit 137c21e171b13cdba23ef94276448405be23dd46)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,5 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
-//= require jquery_ujs
-//= require popper
-//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
