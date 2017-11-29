f056f2ddf399fe63039f307a3d29185a5e0a82be
Hiago Koziel <hiagokoziel100@gmail.com>
2017-11-29 01:13:32 +0000

    Removing redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController <ApplicationController
+  
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def new
     @article = Article.new
   end
@@ -8,11 +11,9 @@ class ArticlesController <ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params
     )
       flash[:notice] = "Article was successfully updated"
@@ -23,7 +24,6 @@ class ArticlesController <ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
@@ -40,11 +40,14 @@ class ArticlesController <ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
   end
   
   private 
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+    
+    def set_article
+      @article = Article.find(params[:id])
     end
 end
