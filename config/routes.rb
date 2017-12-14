57b6805274cc2fde6c1268a7be21c4f14d08af98
Hiago Koziel <hiagokoziel100@gmail.com>
2017-12-14 15:35:20 +0000

    inserting login for users

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   
   resources :users, except: [:new]
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 end
