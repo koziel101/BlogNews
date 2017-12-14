0e314b0c1cc26ba9535e8e2bd1f3aa67d13d439a
Hiago Koziel <hiagokoziel100@gmail.com>
2017-12-14 17:16:03 +0000

    Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -44,5 +45,12 @@ class UsersController < ApplicationController
   private
   def set_user
     @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
 end
